a77cd1e2a743b84b696db9bd6017e694530aed47
lshuaishuai <1847107311@qq.com>
2024-04-19 17:04:39 +0800

    线程互斥代码

---
+++ lesson32/Mutex.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+# include <iostream>
+# include <pthread.h>
+
+class Mutex
+{
+public:
+    Mutex(pthread_mutex_t *lock_p = nullptr)
+        :_lock_p(lock_p)
+    {}
+
+    void lock()
+    {
+        if(_lock_p) pthread_mutex_lock(_lock_p);
+    }
+
+    void unlock()
+    {
+        if(_lock_p) pthread_mutex_unlock(_lock_p);
+    }
+
+private:
+    pthread_mutex_t *_lock_p;
+};
+
+class LockGuard
+{
+public:
+
+    LockGuard(pthread_mutex_t *mutex)
+        :_mutex(mutex)
+    {
+        _mutex.lock(); // 在构造函数中进行加锁
+    }
+
+    ~LockGuard()
+    {
+        _mutex.unlock(); // 解锁
+    }
+
+private:
+    Mutex _mutex;
+};
--- lesson32/mythread.cc
+++ lesson32/mythread.cc
@@ -3,11 +3,15 @@
 # include <string>
 # include <cstring>
 # include <memory> 
+# include <vector>
 # include <unistd.h>
 # include <pthread.h>
 # include "Thread.hpp"
+# include "Mutex.hpp"
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+#define NUM 4
+
+// pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 // 共享资源
 int tickets = 10000;
@@ -24,41 +28,96 @@ int tickets = 10000;
 // 线程一般在什么时候发生切换？时间片到了/来了更高优先级的线程/或者线程等待的时候
 // 线程是在什么时候检测上面的问题呢？从内核态返回用户态的时候，线程对调度状态进行检测，如果可以，就直接发生线程切换
 
+class ThreadData
+{
+public:
+
+    ThreadData(const std::string &threadname, pthread_mutex_t *mutex_p)
+        :_threadname(threadname)
+        ,_mutex_p(mutex_p)
+    {}
+
+    ~ThreadData(){}
+
+public:
+    std::string _threadname;
+    pthread_mutex_t *_mutex_p;
+};
+
 void *thread_run(void *args)
 {
-    std::string username = static_cast<const char*>(args);
+    // std::string username = static_cast<const char*>(args);
+    ThreadData *td = static_cast<ThreadData*>(args);
     while(true)
     {
-        pthread_mutex_lock(&lock);
-        if(tickets > 0)
+        // 加锁和解锁的过程是由多个线程串行执行的 程序会变慢
+        // 锁之规定互斥访问，并没有规定不许让谁优先执行
+        // pthread_mutex_lock(td->_mutex_p);
         {
-            usleep(1234); // 1s=1000ms=1000 000us
+            LockGuard lockguard(td->_mutex_p);
+            if(tickets > 0)
+            {
+                usleep(1234); // 1s=1000ms=1000 000us
+                // std::cout  << username << " 正在进行抢票:" << tickets << std::endl;
 
-            std::cout  << username << " 正在进行抢票:" << tickets << std::endl;
-            // 用这段时间来模拟真实抢票花费的时间
-            tickets--;
-            pthread_mutex_unlock(&lock);
-        }
-        else
-        {
-            pthread_mutex_unlock(&lock);
-            break;
+                std::cout  << td->_threadname << " 正在进行抢票:" << tickets << std::endl;
+                // 用这段时间来模拟真实抢票花费的时间
+                tickets--;
+                // pthread_mutex_unlock(td->_mutex_p);
+            }
+            else 
+            {
+                // pthread_mutex_unlock(td->_mutex_p);
+                break; 
+            }  
         }        
 
-        // 抢完票就完了吗？
+        // 抢完票就完了吗？ 模拟抢完票之后的流程
+        usleep(1000);
     }
     return nullptr;
 }
 
 int main()
 {
-    std::unique_ptr<Thread> thread1(new Thread(thread_run, (void*)"helloThread", 0)); 
-    std::unique_ptr<Thread> thread2(new Thread(thread_run, (void*)"countThread", 1)); 
-    std::unique_ptr<Thread> thread3(new Thread(thread_run, (void*)"logThread", 2)); 
+    pthread_mutex_t lock;
+    pthread_mutex_init(&lock, nullptr);
+    std::vector<pthread_t> tids(NUM);
+    for(int i = 1; i <= NUM; i++)
+    {
+        char namebuffer[64];
+        snprintf(namebuffer, sizeof(namebuffer), "thread %d", i);
+        ThreadData *td = new ThreadData(namebuffer, &lock);
+        pthread_create(&tids[i-1], nullptr, thread_run, td);
+    }
 
-    thread1->join();
-    thread2->join();
-    thread3->join();
+    for(const auto &tid:tids)
+    {
+        pthread_join(tid, nullptr);
+    }
+
+    
+    // pthread_t t1, t2, t3, t4;
+    // pthread_create(&t1, nullptr, thread_run, (void*)"thread 1");
+    // pthread_create(&t2, nullptr, thread_run, (void*)"thread 2");
+    // pthread_create(&t3, nullptr, thread_run, (void*)"thread 3");
+    // pthread_create(&t4, nullptr, thread_run, (void*)"thread 4");
+    
+    // pthread_join(t1, nullptr);
+    // pthread_join(t2, nullptr);
+    // pthread_join(t3, nullptr);
+    // pthread_join(t4, nullptr);
+
+    pthread_mutex_destroy(&lock);
+
+
+    // std::unique_ptr<Thread> thread1(new Thread(thread_run, (void*)"helloThread", 0)); 
+    // std::unique_ptr<Thread> thread2(new Thread(thread_run, (void*)"countThread", 1)); 
+    // std::unique_ptr<Thread> thread3(new Thread(thread_run, (void*)"logThread", 2)); 
+
+    // thread1->join();
+    // thread2->join();
+    // thread3->join();
 
     return 0;
 }
